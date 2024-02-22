14cf9b79277f33e77d66f3178acbf460392b041e
sumit singh tomar <sumitsinght10@gmail.com>
2024-02-22 09:34:22 +0530

    Connect With Backend

---
+++ index.js
@@ -0,0 +1,22 @@
+async function handleSubmit(event) {
+    event.preventDefault()
+
+    var userDetail = {
+        user_name: event.target.user_name.value,
+        email_id: event.target.email_id.value,
+        password: event.target.password.value
+    }
+
+    try {
+        const result = await axios.post('http://localhost:3000/register/user-register', userDetail)
+        if (result.data.status) {
+            alert('user successful login')
+        }
+    }
+    catch (e) {
+        alert(e.response.data.data)
+    }
+    event.target.user_name.value='',
+    event.target.email_id.value='',
+    event.target.password.value=''
+}
