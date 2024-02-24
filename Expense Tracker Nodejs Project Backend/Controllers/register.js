3b471a16d595619417b0bb8c546610132f28fb5c
sumit singh tomar <sumitsinght10@gmail.com>
2024-02-24 10:43:19 +0530

    adding bcrypt for password security

--- Expense Tracker Nodejs Project Backend/Controllers/register.js
+++ Expense Tracker Nodejs Project Backend/Controllers/register.js
@@ -1,18 +1,23 @@
 const db = require('../Connections/database')
+const bcrypt = require('bcrypt')
 
 exports.userRegisteration = (req, res) => {
     const query = "INSERT INTO admins (name, emailid, password) VALUES (?,?,?)"
-    const values = [req.body.user_name, req.body.email_id, req.body.password]
-    db.execute(query, values)
-        .then((result) => {
-            res.status(200).json({ status: true, data: 'User Registeration Succesfully' })
-        })
-        .catch((e) => {
-            if (e.code == 'ER_DUP_ENTRY')
-                res.status(500).json({ status: false, data: 'User Already Exist' })
-            else
-                res.status(500).json({ status: false, data: 'Server Error' })
-        })
+    bcrypt.hash(req.body.password, 10, (err, hash) => {
+        console.log('hasherror', err);
+        console.log('hash', hash);
+        db.execute(query, [req.body.user_name, req.body.email_id, hash])
+            .then((result) => {
+                res.status(200).json({ status: true, data: 'User Registeration Succesfully' })
+            })
+            .catch((e) => {
+                if (e.code === 'ER_DUP_ENTRY')
+                    res.status(500).json({ status: false, data: 'User Already Exist' })
+                else
+                    res.status(500).json({ status: false, data: 'Server Error' })
+            })
+    })
+
 }
 
 exports.loginUser = (req, res) => {
@@ -24,12 +29,14 @@ exports.loginUser = (req, res) => {
                 res.status(404).json({ status: false, data: 'User Not Found' })
             }
             else {
-                if (result[0][0].password == req.body.password) {
+                bcrypt.compare(req.body.password, result[0][0].password, (err, response) => {
+                    if(err)
+                    res.status(401).json({ status: false, data: 'User not Authorized, Incorrect Password' })
+                    else
                     res.status(200).json({ status: true, data: result[0] })
-                }
-                else{
-                    res.status(401).json({status:false,data:'User not Authorized, Incorrect Password'})
-                }
+
+                })
+
             }
         })
         .catch((e) => {
