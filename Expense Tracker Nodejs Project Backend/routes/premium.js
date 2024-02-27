8f5e5e7274351841a20eb2aac06e114aaf8972b6
Sumit-singh-tomar <142825774+Sumit-singh-tomar@users.noreply.github.com>
2024-02-27 21:30:04 +0530

    Delete Expense Tracker Nodejs Project Backend directory

--- Expense Tracker Nodejs Project Backend/routes/premium.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const authentication = require('../middleware/auth')
-const premiumController = require('../Controllers/premium')
-
-router.get('/show-leaderboard', authentication, premiumController.showLeaderBoard)
-
-module.exports = router;
